3e0c79b41206263888ed14d6670a41963cf59615
balajiG54 <109754215+balajiG54@users.noreply.github.com>
2022-10-18 00:29:43 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,13 +3,12 @@ import qrcode
 import qrcode.image.svg
 import os
 
-
-def make_qr_quote(data, name, dirname):
+def make_qr_quote(data,name,dirname):
     qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=5,
-        border=2)
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=2)
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color=(150,151,54), back_color="white")
@@ -18,27 +17,27 @@ def make_qr_quote(data, name, dirname):
     # return img.get_image()
 
 
-def make_qr_svg_quote(data, name, dirname):
+def make_qr_svg_quote(data,name,dirname):
     qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage)
     qr.add_data(data)
     qr.make(fit=True)
+    # img = qr.make_image(fill='#000000')
 
     img = qr.make_image(fill='#969736')
     os.makedirs(f"./result/{dirname}/{dirname}_qr_images/", exist_ok=True)
     img.save(f"./result/{dirname}/{dirname}_qr_images/{name}.svg")
 
 
-def make_AR_qr_svg_quote(data, name, dirname):
+def make_AR_qr_svg_quote(data,name,dirname):
     qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage)
     qr.add_data(data)
     qr.make(fit=True)
-    img = qr.make_image(fill='#969736')
+    img = qr.make_image(fill='#d12027')
     os.makedirs(f"./result/{dirname}/{dirname}_qr_images/", exist_ok=True)
     img.save(f"./result/{dirname}/{dirname}_qr_images/{name}.svg")
 
 
-def v_card(dirname, company, first_name, last_name, title, email, office_number="", mobile_number="", address=None,
-           zip_code=None):
+def v_card(dirname, company, first_name, last_name, title, email, office_number = "" , mobile_number = "", address = None, zip_code = None):
     if company == "GetixHealth":
         make_qr_svg_quote(f'''BEGIN:VCARD
 VERSION:3.0
@@ -63,7 +62,7 @@ TEL;TYPE=voice,CELL,pref:{mobile_number}
 
 ADR;TYPE=dom,work,postal,parcel:;;{address};;{zip_code};
 
-END:VCARD ''', first_name + " " + last_name, dirname)
+END:VCARD ''',first_name + " " + last_name, dirname)
 
     else:
         make_AR_qr_svg_quote(f'''BEGIN:VCARD
@@ -89,4 +88,4 @@ TEL;TYPE=voice,CELL,pref:{mobile_number}
 
 ADR;TYPE=dom,work,postal,parcel:;;{address};;{zip_code};
 
-END:VCARD ''', first_name + " " + last_name, dirname)
+END:VCARD ''',first_name + " " + last_name, dirname)
